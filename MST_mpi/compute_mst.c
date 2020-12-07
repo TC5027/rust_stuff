4d5e8fb01b757b00dfcbec1a5853fe26ca442c96
TC5027 <thibert501@gmail.com>
2020-12-07 17:31:48 +0100

    mst complete graph

---
+++ MST_mpi/compute_mst.c
@@ -0,0 +1,141 @@
+// mpicc compute_mst.c -o compute_mst -lrustc -L./rustc/target/debug
+// LD_LIBRARY_PATH=rustc/target/debug/ mpirun --use-hwthread-cpus -np 3 ./compute_mst
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+extern void boruvka(int *subgraph, int subgraph_length, int size, int size_global, int shift, int *output, int output_lenght);
+const int N = 6;
+
+struct subset {
+	int parent;
+	int rank;
+};
+int find(struct subset subsets[], int i) {
+	if (subsets[i].parent != i) {
+		subsets[i].parent = find(subsets, subsets[i].parent);
+	}
+	return subsets[i].parent;
+}
+void Union(struct subset subsets[], int x, int y) {
+	int xroot = find(subsets, x);
+	int yroot = find(subsets, y);
+	if (subsets[xroot].rank<subsets[yroot].rank) {
+		subsets[xroot].parent = yroot;
+	} else if (subsets[xroot].rank>subsets[yroot].rank) {
+		subsets[yroot].parent = xroot;
+	}
+	else {
+		subsets[yroot].parent = xroot;
+		subsets[xroot].rank++;
+	}
+}
+void kruskal(int edges[2*N-3][2]) {
+	int e = 0;
+	int i = 0;
+	int MST[N-1][2];
+	
+	struct subset* subsets = (struct subset*) malloc(N*sizeof(struct subset));
+	for (int v = 0; v<N; v++) {
+		subsets[v].parent = v;
+		subsets[v].rank = 0;
+	}
+	
+	while (e<N-1) {
+		int u = edges[i][0];
+		int v = edges[i][1];
+		int subset_u = find(subsets, u);
+		int subset_v = find(subsets, v);
+		//subset_u != subset_v => adding this edges doesn't cause cycle
+		if (subset_u != subset_v) {
+			MST[e][0] = u;
+			MST[e][1] = v;
+			Union(subsets, subset_u, subset_v);
+			e++;
+		}
+		i++;
+	}
+	for (int i = 0; i<N-2; i++) {
+		printf("%d",MST[i][0]);
+		printf("-%d/",MST[i][1]);
+	}
+	printf("%d",MST[N-2][0]);
+	printf("-%d\n",MST[N-2][1]);
+}
+
+int main(int argc, char *argv[]) {
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	int s = N*2/3;
+	int subgraph[3*s*(s-1)];
+	int local_MST[2*(s-1)];
+	int *combined_MST = NULL;
+	int **graph = NULL;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	if (rank == 0) {
+		combined_MST = (int *)malloc(size*2*(s-1)*sizeof(int));
+		//int combined_MST[3*(2*(s-1))];
+		graph = (int **)malloc(N*sizeof(int *));
+		for (int i=0; i<N; i++)
+			graph[i] = (int *)malloc((N-1)*sizeof(int));
+		graph[0][0] = 0;graph[0][1] = 4;graph[0][2] = 4;graph[0][3] = 4;graph[0][4] = 1;
+		graph[1][0] = 0;graph[1][1] = 2;graph[1][2] = 4;graph[1][3] = 4;graph[1][4] = 4;
+		graph[2][0] = 4;graph[2][1] = 2;graph[2][2] = 0;graph[2][3] = 4;graph[2][4] = 4;
+		graph[3][0] = 4;graph[3][1] = 4;graph[3][2] = 0;graph[3][3] = 3;graph[3][4] = 4;
+		graph[4][0] = 4;graph[4][1] = 4;graph[4][2] = 4;graph[4][3] = 3;graph[4][4] = 0;
+		graph[5][0] = 1;graph[5][1] = 4;graph[5][2] = 4;graph[5][3] = 4;graph[5][4] = 0;
+		//int graph[6][5] = {{0,4,4,4,1},{0,2,4,4,4},{4,2,0,4,4},{4,4,0,3,4},{4,4,4,3,0},{1,4,4,4,0}}; 
+		for (int k = 1; k<size; k++) {
+			for (int i = 0; i<s; i++) {
+				for (int j = 0; j<s-1; j++){
+					subgraph[3*(i*(s-1)+j)] = i;
+					if (i<=j) {
+						subgraph[3*(i*(s-1)+j)+1] = j+1;
+					} else {
+						subgraph[3*(i*(s-1)+j)+1] = j;
+					}
+					subgraph[3*(i*(s-1)+j)+2] = graph[(i+N/3*k)%N][(j+N/3*k)%N];
+				}
+			}
+			MPI_Send(subgraph, 3*s*(s-1), MPI_INT, k, 0, MPI_COMM_WORLD);
+		}
+		for (int i = 0; i<s; i++) {
+			for (int j = 0; j<s-1; j++) {
+				subgraph[3*(i*(s-1)+j)] = i; 
+				if (i<=j) {
+					subgraph[3*(i*(s-1)+j)+1] = j+1;
+				} else {
+					subgraph[3*(i*(s-1)+j)+1] = j;
+				}
+				subgraph[3*(i*s+j)+2] = graph[i][j];
+			}
+		}
+	}
+	else {
+		MPI_Recv(subgraph, 3*s*(s-1), MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);		
+	}
+	for (int k = 0; k<size; k++) {
+		if (rank == k ) {
+			boruvka(subgraph, 3*s*(s-1), s, N, k*N/3, local_MST, 2*(s-1));
+		}
+	}
+	MPI_Gather(local_MST, 2*(s-1), MPI_INT, combined_MST, 2*(s-1), MPI_INT,0, MPI_COMM_WORLD);
+	if (rank == 0) {
+		int reshaped_combined_MST[3*(s-1)][2];
+		for (int i = 0; i<3*(s-1); i++) {
+			reshaped_combined_MST[i][0] = combined_MST[2*i];
+			reshaped_combined_MST[i][1] = combined_MST[2*i+1];
+		}
+		int compare (const void *p1, const void *p2) {
+			int *arr1 = (int*)p1;
+			int *arr2 = (int*)p2;
+			return graph[arr1[1]][arr1[0]]-graph[arr2[1]][arr2[0]];
+	}
+	qsort(reshaped_combined_MST, 3*(s-1), 2*sizeof(int), compare);
+	kruskal(reshaped_combined_MST);
+	}
+	MPI_Finalize();
+	return 0;
+}
---
+++ MST_mpi/rustc/src/lib.rs
@@ -0,0 +1,116 @@
+use std::slice;
+use rayon::prelude::*;
+use std::collections::{HashMap};
+use std::cmp::{min,max};
+
+/// boruvka algorithm, designed for complete graph
+#[no_mangle]
+pub extern "C" fn boruvka(c_array: *mut i32, length: usize, size: usize, size_global: usize, shift: usize, output: *mut i32, output_length: usize) {
+    let edges: &mut [i32] = unsafe{slice::from_raw_parts_mut(c_array, length as usize)};
+    let mut edges = edges.par_chunks(3)
+            .map(|v| (v[0] as usize, v[1] as usize, v[2]))
+            .collect::<Vec<(usize,usize,i32)>>();
+    // where we store the edges of MST, one edge = a pair of vertices
+    let mut tree = HashMap::new();
+    // number of composants in the current state of the graph
+    let mut composants = size;
+    // keeps track to which components each vertex belongs
+    let mut belongs_to = HashMap::new();
+    // initially, one vertex = one component
+    for k in 0..size {
+        belongs_to.insert(k,k);
+    }
+    while composants > 1 {
+        // STEP search for the lightest edges between distinct components
+        let found = edges.par_chunks(composants-1).map(|chunk| {
+            let mut mini = std::i32::MAX;
+            let mut index = 0;
+            for (k,(_,_,w_ij)) in chunk.iter().enumerate() {
+                if mini > *w_ij {
+                    mini = *w_ij;
+                    index = k;
+                }
+            }
+            chunk[index]
+        }).collect::<Vec<_>>();
+        
+        // insert the found edges of the MST
+        found.iter().for_each(|&(i,j,_)| {
+            tree.insert((min(i,j),max(i,j)), 1);
+        });
+
+        // STEP assign subgraph to vertices
+        let mut pred = found.par_iter()
+                .map(|(i,j,_)| (belongs_to[i],belongs_to[j]))
+                .collect::<HashMap<_,_>>();
+        pred = (0..composants).into_par_iter().map(|c| {
+            let w = pred[&c];
+            if pred[&w]==c && c<w {
+                (c,c)
+            } else {
+                (c,w)
+            }
+        }).collect();
+        while (0..composants).into_par_iter().any(|c| pred[&c]!=pred[&pred[&c]]) {
+            pred = (0..composants).into_par_iter().map(|c| {
+                (c,pred[&pred[&c]])
+            }).collect();
+        }
+        // like that all components of same new component have same values in pred
+
+        // we update the number of remaining components
+        composants = size-tree.len();
+
+        // remove the edges within the same component
+        let not_within_same = edges.into_par_iter()
+                .filter(|(i,j,_)| pred[&belongs_to[i]]!=pred[&belongs_to[j]])
+                .collect::<Vec<_>>();
+
+        // concordance helps us mapping the index of components to the range 0..composants
+        let mut concordance = pred.values()
+                .map(|v| (v.clone(),1 as usize))
+                .collect::<HashMap<_,_>>();
+        concordance = concordance.keys().enumerate().map(|(i,&j)| (j,i)).collect::<HashMap<_,_>>();
+        
+        // organize the edges to preserve order so that next iteration 
+        // we can re-use the "par_chunks(composants-1)" for found
+        let organize_by_component = (0..composants).into_par_iter()
+                .map(|k| {
+                    not_within_same.iter()
+                    .filter(|(i,_,_)| concordance[&pred[&i]] == k)
+                    .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+        
+        // we remove the multi-edges between the different components
+        // meaning we don't want to have more than 1 edge between 2 distinct components
+        edges = (0..composants).into_par_iter().map(|k| {
+            let mut minis = vec![(0,0,std::i32::MAX);composants];
+            for &(i,j,w_ij) in organize_by_component[k].clone() {
+                let composant = concordance[&pred[&j]];
+                if minis[composant].2 > w_ij {
+                    minis[composant] = (i,j,w_ij);
+                }
+            }
+            minis.into_par_iter()
+        }).flatten().filter(|(i,j,_)| i!=j).collect();
+        
+        // we keep the data concerning the component's attributions for each vertex in belongs_to
+        for k in 0..size {
+            belongs_to.insert(k,concordance[&pred[&belongs_to[&k]]]);
+        }
+    }
+    let output: &mut [i32] = unsafe{slice::from_raw_parts_mut(output, output_length)};
+    for (k,(i,j)) in tree.keys().enumerate() {
+        output[2*k] = ((i+shift)%size_global) as i32;
+        output[2*k+1] = ((j+shift)%size_global) as i32;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
