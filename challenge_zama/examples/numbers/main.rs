b8dff9eb08a050d3234987e280e35c1096cdef3e
TC5027 <thibert501@gmail.com>
2021-05-25 20:28:12 +0200

    comments + clippy + rustfmt

--- challenge_zama/examples/numbers/main.rs
+++ challenge_zama/examples/numbers/main.rs
@@ -6,17 +6,17 @@ mod parameters;
 use dataset::DATASET;
 use parameters::*;
 
-fn custom_print(number : &[f64;784]) {
+fn custom_print(number: &[f64; 784]) {
     for i in 0..28 {
         let mut string = String::new();
         for j in 0..28 {
-            if number[28*i+j]>0.5 {
+            if number[28 * i + j] > 0.5 {
                 string.push('.');
             } else {
                 string.push(' ');
             }
         }
-        println!("{}",string);
+        println!("{}", string);
     }
 }
 
@@ -25,28 +25,29 @@ fn main() {
         println!("number seen : ");
         custom_print(&number);
 
-        let mut weight_1 = Matrix::new(28*28,256,&WEIGHT_1);
+        let mut weight_1 = Matrix::new(28 * 28, 256, &WEIGHT_1);
         weight_1.transpose();
-        let bias_1 = Matrix::new(256,1,&BIAS_1);
+        let bias_1 = Matrix::new(256, 1, &BIAS_1);
 
-        let mut weight_2 = Matrix::new(256,32,&WEIGHT_2);
+        let mut weight_2 = Matrix::new(256, 32, &WEIGHT_2);
         weight_2.transpose();
-        let bias_2 = Matrix::new(32,1,&BIAS_2);
+        let bias_2 = Matrix::new(32, 1, &BIAS_2);
 
-        let mut weight_3 = Matrix::new(32,10,&WEIGHT_3);
+        let mut weight_3 = Matrix::new(32, 10, &WEIGHT_3);
         weight_3.transpose();
-        let bias_3 = Matrix::new(10,1,&BIAS_3);
+        let bias_3 = Matrix::new(10, 1, &BIAS_3);
 
-        let mut matrix = Matrix::new(28,28,number);
+        let mut matrix = Matrix::new(28, 28, number);
         matrix.flatten();
-        matrix.linear_combination(weight_1,bias_1);
+        matrix.linear_combination(weight_1, bias_1);
         matrix.relu();
-        matrix.linear_combination(weight_2,bias_2);
+        matrix.linear_combination(weight_2, bias_2);
         matrix.relu();
-        matrix.linear_combination(weight_3,bias_3);
+        matrix.linear_combination(weight_3, bias_3);
         matrix.softmax();
 
-        let mut index = 0; let mut maxi = matrix.data[0];
+        let mut index = 0;
+        let mut maxi = matrix.data[0];
         for i in 1..10 {
             if matrix.data[i] > maxi {
                 index = i;
@@ -54,6 +55,9 @@ fn main() {
             }
         }
 
-        println!("number recognized is {} with value {}",index,matrix.data[index]);
+        println!(
+            "number recognized is {} with value {}",
+            index, matrix.data[index]
+        );
     }
 }
--- challenge_zama/src/lib.rs
+++ challenge_zama/src/lib.rs
@@ -1,33 +1,31 @@
-use std::ops::{Add,Mul};
 use std::iter::Sum;
+use std::ops::{Add, Mul};
 
 /// Matrix object, defined as a contiguous sequence of data
 /// correclty interpreted with the nb_col and nb_row fields.
 /// Accessing matrix[i,j] can be done looking at data[i*nb_col+j]
 /// (with i and j starting at 0)
-#[derive(PartialEq,Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Matrix<T> {
-    pub data : Vec<T>,
-    pub nb_col : usize,
-    pub nb_row : usize
+    pub data: Vec<T>,
+    pub nb_col: usize,
+    pub nb_row: usize,
 }
 
-impl<T:Copy> Matrix<T>  {
-    /// Creates a Matrix object.
-    /// We accept as input for the data anything that implements
-    /// the trait IntoIterator.
+impl<T: Copy> Matrix<T> {
+    /// Creates a Matrix object, data being flattened row wise.
     /// nb_col and nb_row are expected to be > 0 and data length
     /// must be equal to nb_col and nb_row.
-    pub fn new (nb_col : usize, nb_row : usize, data : &[T]) -> Self {
-        let data : Vec<T> = data.iter().copied().collect();
-        
-        assert_eq!(nb_col*nb_row, data.len());
+    pub fn new(nb_col: usize, nb_row: usize, data: &[T]) -> Self {
+        let data: Vec<T> = data.iter().copied().collect();
+
+        assert_eq!(nb_col * nb_row, data.len());
         assert!(nb_col > 0 && nb_row > 0);
 
         Matrix {
-            data : data,
-            nb_col : nb_col,
-            nb_row : nb_row
+            data,
+            nb_col,
+            nb_row,
         }
     }
 
@@ -36,86 +34,125 @@ impl<T:Copy> Matrix<T>  {
         self.nb_col *= self.nb_row;
         self.nb_row = 1;
     }
-}
 
-impl<T:Copy+Add<Output=T>+Mul<Output=T>+Sum> Matrix<T> {
-
-    pub fn convolution(&mut self, kernel : Matrix<T>) {
-        let previous_nb_col = self.nb_col;
-
-        self.nb_col -= kernel.nb_col - 1;
-        self.nb_row -= kernel.nb_row - 1;
-    
-        self.data = (0usize..self.nb_col*self.nb_row).map(|k| {
-            let (i,j) = (k/self.nb_col,k%self.nb_col);
-            let value = kernel.data.iter()
-                .enumerate()
-                .map(|(index,&x)| {
-                    let (k_i,k_j) = (index/kernel.nb_col,index%kernel.nb_col);
-                    x*self.data[(i+k_i)*previous_nb_col + (j+k_j)]
-                })
-                .sum();
-            value
-        }).collect();
-    }
-
-    pub fn linear_combination(&mut self, weights : Matrix<T>, bias : Matrix<T>) {
-        self.data = (0..weights.nb_col).map(|j| {
-            self.data.iter()
-                .enumerate()
-                .map(|(index,&x)| x*weights.data[index*weights.nb_col+j])
-                .sum::<T>() + bias.data[j]
-        }).collect();
-
-        self.nb_col = bias.nb_col;
-    }
-}
-
-impl<T:Copy> Matrix<T> {
-
+    /// Out of place transposition of a Matrix object
     pub fn transpose(&mut self) {
         let nb_col = self.nb_row;
         let nb_row = self.nb_col;
 
-        let mut data = Vec::new();
+        let mut new_data = Vec::new();
         for i in 0..nb_row {
             for j in 0..nb_col {
-                data.push(self.data[j*self.nb_col+i]);
+                new_data.push(self.data[j * self.nb_col + i]);
             }
         }
 
         self.nb_col = nb_col;
         self.nb_row = nb_row;
-        self.data = data;
+        self.data = new_data;
+    }
+}
+
+impl<T: Copy + Add<Output = T> + Mul<Output = T> + Sum> Matrix<T> {
+    /// Convolution by a kernel which is represented as
+    /// another Matrix object, without strides and padding.
+    pub fn convolution(&mut self, kernel: Matrix<T>) {
+        assert!(self.nb_col >= kernel.nb_col && self.nb_row >= kernel.nb_row);
+        // we remember the previous number of columns
+        // of self, to navigate correctly inside its
+        // data field
+        let previous_nb_col = self.nb_col;
+
+        // we set the new dimensions
+        self.nb_col -= kernel.nb_col - 1;
+        self.nb_row -= kernel.nb_row - 1;
+
+        self.data = (0usize..self.nb_col * self.nb_row)
+            .map(|k| {
+                // we go through each position of the result
+                // matrix one by one
+                let (i, j) = (k / self.nb_col, k % self.nb_col);
+                // we compute the value at this position
+                kernel
+                    .data
+                    .iter()
+                    .enumerate()
+                    .map(|(index, &x)| {
+                        let (k_i, k_j) = (index / kernel.nb_col, index % kernel.nb_col);
+                        x * self.data[(i + k_i) * previous_nb_col + (j + k_j)]
+                    })
+                    .sum()
+            })
+            .collect();
+    }
+
+    /// Linear combination by a weight (Matrix object) and
+    /// a bias (flattened Matrix object) of a flattened self.
+    pub fn linear_combination(&mut self, weights: Matrix<T>, bias: Matrix<T>) {
+        // assert self and bias respect the dimension's
+        // constraint of the method
+        assert!(self.nb_row == 1 && bias.nb_row == 1);
+        // assert the weights' dimensions match the ones of
+        // self and bias
+        assert!(self.nb_col == weights.nb_row && bias.nb_col == weights.nb_col);
+
+        self.data = (0..weights.nb_col)
+            .map(|j| {
+                // we go through each position of the result
+                // matrix one by one and compute its value
+                self.data
+                    .iter()
+                    .enumerate()
+                    .map(|(index, &x)| x * weights.data[index * weights.nb_col + j])
+                    .sum::<T>()
+                    + bias.data[j]
+            })
+            .collect();
+
+        self.nb_col = bias.nb_col;
     }
 }
 
 impl Matrix<f64> {
-
+    /// Apply hyperbolic tangent on Matrix's data
     pub fn tanh(&mut self) {
         self.data = self.data.iter().map(|&x| x.tanh()).collect();
     }
 
+    /// Apply RELU on Matrix's data
     pub fn relu(&mut self) {
         self.data = self.data.iter().map(|&x| x.max(0.0)).collect();
     }
 
+    /// Apply softmax on Matrix's data
     pub fn softmax(&mut self) {
-        let sum : f64 = self.data.iter().map(|&x| x.exp()).sum();
-        self.data = self.data.iter().map(|&x| x.exp()/sum).collect();
+        let sum: f64 = self.data.iter().map(|&x| x.exp()).sum();
+        self.data = self.data.iter().map(|&x| x.exp() / sum).collect();
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_convolution() {
-        let kernel = Matrix::new(3,3,&vec![1,0,1,0,1,0,1,0,1]);
-        let mut matrix = Matrix::new(7,7,&vec![0,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0]);
+        let kernel = Matrix::new(3, 3, &vec![1, 0, 1, 0, 1, 0, 1, 0, 1]);
+        let mut matrix = Matrix::new(
+            7,
+            7,
+            &vec![
+                0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+                0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+            ],
+        );
 
-        let expected_output = Matrix::new(5,5,&vec![1,4,3,4,1,1,2,4,3,3,1,2,3,4,1,1,3,3,1,1,3,3,1,1,0]);
+        let expected_output = Matrix::new(
+            5,
+            5,
+            &vec![
+                1, 4, 3, 4, 1, 1, 2, 4, 3, 3, 1, 2, 3, 4, 1, 1, 3, 3, 1, 1, 3, 3, 1, 1, 0,
+            ],
+        );
 
         matrix.convolution(kernel);
 
@@ -124,21 +161,21 @@ mod tests {
 
     #[test]
     fn test_linear_combination() {
-        let mut one_dim = Matrix::new(3,1,&vec![3,4,2]);
-        let weights = Matrix::new(4,3,&vec![13,9,7,15,8,7,4,6,6,4,0,3]);
-        let bias = Matrix::new(4,1,&vec![1,1,1,1]);
+        let mut one_dim = Matrix::new(3, 1, &vec![3, 4, 2]);
+        let weights = Matrix::new(4, 3, &vec![13, 9, 7, 15, 8, 7, 4, 6, 6, 4, 0, 3]);
+        let bias = Matrix::new(4, 1, &vec![1, 1, 1, 1]);
 
-        let expected_output = Matrix::new(4,1,&vec![84,64,38,76]);
+        let expected_output = Matrix::new(4, 1, &vec![84, 64, 38, 76]);
 
         one_dim.linear_combination(weights, bias);
 
         assert_eq!(one_dim, expected_output);
 
-        let mut one_dim = Matrix::new(3,1,&vec![1,2,3]);
-        let weights = Matrix::new(3,3,&vec![2,1,3,3,3,2,4,1,2]);
-        let bias = Matrix::new(3,1,&vec![-10,0,-3]);
+        let mut one_dim = Matrix::new(3, 1, &vec![1, 2, 3]);
+        let weights = Matrix::new(3, 3, &vec![2, 1, 3, 3, 3, 2, 4, 1, 2]);
+        let bias = Matrix::new(3, 1, &vec![-10, 0, -3]);
 
-        let expected_output = Matrix::new(3,1,&vec![10,10,10]);
+        let expected_output = Matrix::new(3, 1, &vec![10, 10, 10]);
 
         one_dim.linear_combination(weights, bias);
 
@@ -147,19 +184,19 @@ mod tests {
 
     #[test]
     fn activation_functions() {
-        let mut matrix = Matrix::new(3,1,&vec![3.0,-4.0,0.0]);
+        let mut matrix = Matrix::new(3, 1, &vec![3.0, -4.0, 0.0]);
         matrix.relu();
 
-        let expected_output = Matrix::new(3,1,&vec![3.0,0.0,0.0]);
+        let expected_output = Matrix::new(3, 1, &vec![3.0, 0.0, 0.0]);
         assert_eq!(matrix, expected_output);
     }
 
     #[test]
     fn test_transpose() {
-        let mut matrix = Matrix::new(3,2,&vec![1,2,3,4,5,6]);
+        let mut matrix = Matrix::new(3, 2, &vec![1, 2, 3, 4, 5, 6]);
         matrix.transpose();
 
-        let expected_output = Matrix::new(2,3,&vec![1,4,2,5,3,6]);
+        let expected_output = Matrix::new(2, 3, &vec![1, 4, 2, 5, 3, 6]);
         assert_eq!(matrix, expected_output);
     }
 }
