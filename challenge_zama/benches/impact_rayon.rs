e81ad806f1fca6140864bdbe1c83a4ee5b550679
TC5027 <thibert501@gmail.com>
2021-05-30 13:56:29 +0200

    same for rayon version

--- challenge_zama/benches/impact_rayon.rs
+++ challenge_zama/benches/impact_rayon.rs
@@ -6,9 +6,9 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     for &size in [1_000usize, 2_000, 3_000].iter() {
         let weight = Matrix::new(size, size, &vec![1; size * size]);
-        let bias = Matrix::new(size, 1, &vec![1; size]);
+        let bias = Matrix::new(1, size, &vec![1; size]);
 
-        let mut matrix = Matrix::new(size, 1, &vec![1; size]);
+        let mut matrix = Matrix::new(1, size, &vec![1; size]);
         group_linear_combination.bench_with_input(
             BenchmarkId::new("without Rayon", size),
             &size,
@@ -19,7 +19,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             },
         );
 
-        let mut matrix = Matrix::new(size, 1, &vec![1; size]);
+        let mut matrix = Matrix::new(1, size, &vec![1; size]);
         group_linear_combination.bench_with_input(
             BenchmarkId::new("with Rayon", size),
             &size,
